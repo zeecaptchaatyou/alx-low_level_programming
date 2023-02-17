fd3b7bfc7bdabaa72dd12654416fe0250e44987d
Zainab Jimoh <zeecaptcha@zee.local>
2023-02-17 05:05:57 +0100

    C!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/**
+ * main is the the body
+ */
 int main(void)
 {
 char ALX[] = "\"Programming is like building a multilingual puzzle\n\"";
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - main block
+ * Return; 0
+ */
 int main(void)
-/** 
- *main is body
-*/
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
