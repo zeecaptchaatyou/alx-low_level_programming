2dc96ed972e8b50301492764d77b1063fef144a9
Zainab Jimoh <zeecaptcha@zee.local>
2023-02-17 20:21:21 +0100

    C!

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-/**
- * Definition: main - Prints strings to sdout
- * Return: 0 if compiled successfully
+/*
+ * Definition: main -Prints formatted string to standard output
+ * Return: 0 if succesfully compiled
  */
 
 int main(void)
 {
-	printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
 	return (0);
 }
