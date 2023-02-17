ffb44a0735388603e0ab11ee29f44248bf8e63f4
Zainab Jimoh <zeecaptcha@zee.local>
2023-02-17 04:56:37 +0100

    C!

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+printf("Size of a char:%ld bytes(s)", sizeof(char));
+printf("Size of an int:%ld byte(s)", sizeof(int));
+printf("Size of a long int:%ld byte(s)", sizeof(long int));
+printf("Size of a long long int:%ld byte(s)", sizeof(long long int));
+printf("Size of a float:%ld byte(s)", sizeof(float));
+
+return (0);
+}
