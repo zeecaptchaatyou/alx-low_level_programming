a6740bddca0509e22ce3040a70c7c5ef1d43fd1a
Zainab Jimoh <zeecaptcha@zee.local>
2023-02-17 22:26:00 +0100

    C!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Definition: main - Starts the program
+ * Return: 0 if program compiles successfully
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
