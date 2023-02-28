c319ecce8e542444468fea16a88d02fce010cfd1
Zainab Jimoh <zeecaptcha@zee.local>
2023-02-28 20:05:34 +0100

    Pointers in C

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -16,3 +16,7 @@ int main(void)
     printf("n=%d\n", n);
     return (0);
 }
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98
+julien@ubuntu:~/0x05$ ./0-98 
+n=402
+n=98
