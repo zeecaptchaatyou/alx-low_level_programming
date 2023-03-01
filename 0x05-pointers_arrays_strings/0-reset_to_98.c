5388bd27be1e7b58b7d9163731163676baf6af4f
dev_zee <zeecaptcha@gmail.com>
2023-03-01 03:30:27 +0100

    Pointers in C

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 /* Program to take a pointer to an int, and update the value it points to
  * Author : dev_zee
- * Main : Takes control at the beginning of code compilation and execution
- */
+ * Main : Takes control at the beginning of code compilation and execution, is defined in another file.
+ * Function reseet_to_98 : updates the value of a variable pointed to by pointer n  
+ **/
 
 void reset_to_98(int *n)
 {
