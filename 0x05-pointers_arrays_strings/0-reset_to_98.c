4237ea5e9359aa262e1a88fb8db40db0c4f9c211
dev_zee <zeecaptcha@gmail.com>
2023-03-01 03:46:25 +0100

    Pointers in C

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 /*
  * Program to take a pointer to an int, and update the value it points to
  * Author : dev_zee
- * reset_to_98 - Entry function
+ * reset_to_98 - updates value of pointer to 98.
  * n: pointer to a variable of type int
  */
 
