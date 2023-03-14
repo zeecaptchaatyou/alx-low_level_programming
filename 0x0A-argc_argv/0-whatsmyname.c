4813732163f5ae7a1f9e643968f830a84c8cf21a
Zainab Jimoh <zeecaptcha@zee.local>
2023-03-14 12:55:59 +0100

    Last try

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main: beginning of the program's compilation
+* main - beginning of the program's compilation
 *@argc: argument count
 *@argv: value at each count of argument
 *Return: 0
@@ -10,5 +10,5 @@ int main(int __attribute__((_unused_)) argc, char *argv[])
 {
 	   printf("%s\n", argv[0]);
 	   
-	   return 0;
+	   return (0);
 }
