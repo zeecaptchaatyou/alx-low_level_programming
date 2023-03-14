bee5217f072eebd5601c038dc80c8fc6a76ec8ff
Zainab Jimoh <zeecaptcha@zee.local>
2023-03-14 10:22:31 +0100

    First try

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* argc = argument count
+ * argv = value at each count of argument
+ * Let's do this!
+ */
+   int main(int argc, char **argv[]) {
+	
+	printf("%s", argv[0]);
+
+	return 0;
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+Ever wondered what the argc and argv yadayada passed into the main function at the beginning of some programs you see out there actually means? 
+If your answer is yes then:
+
+Welcome to my repo stranger!
+
+
