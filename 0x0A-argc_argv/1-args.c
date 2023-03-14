a774c52b215759a6421cb54ad6befbc1959a77db
Zainab Jimoh <zeecaptcha@zee.local>
2023-03-14 19:04:02 +0100

    First try

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
 * main - prints number of arguments passed into it
 *@argc: argument count
 *@argv: argument values
-*Return: 0
+*Return: value of argc
 */
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
