eb62e3b943225d3bbca278ccdf10807ff874c09f
Zainab Jimoh <zeecaptcha@zee.local>
2023-03-14 12:45:16 +0100

    Tried dome other solution to bypass the unused variable error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#define UNUSED(argc) (void)(argc)
 
 /**
- *main: takes control of the program first
- *@argc: argument count
- *@argv: value at each count of argument
- *Return: 0
- */
-  
+* main - prints program name
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	   UNUSED(argc);
+	printf("%s\n", argv[0]);
 
-	   printf("%s\n", argv[0]);
-	   
-	   return (0);
+	return (0);
 }
