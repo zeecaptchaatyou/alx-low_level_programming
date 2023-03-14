44f0a8751b0e2bb131ee89210c56cd1efacf24ed
Zainab Jimoh <zeecaptcha@zee.local>
2023-03-14 13:21:08 +0100

    Last try

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
 * main - prints program name
-*@argc: argument count
-*@argv: value at each count of argument
+*@argc: number of arguments
+*@argv: array of arguments
 *Return: 0
-*/  
-int main(int __attribute__((__unused__)) argc, char *argv[]) 
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	   
+
 	return (0);
 }
---
+++ 0x0A-argc_argv/0-whatsmyname.y
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - prints program name
+*@argc: argument count
+*@argv: value at each count of argument
+*Return: 0
+*/  
+int main(int __attribute__((__unused__)) argc, char *argv[]) 
+{
+	printf("%s\n", argv[0]);
+	   
+	return (0);
+}
