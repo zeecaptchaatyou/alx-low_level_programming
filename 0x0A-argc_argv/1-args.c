e574aa6a114418245b099e4a8feac4b5137af42d
Zainab Jimoh <zeecaptcha@zee.local>
2023-03-14 19:09:22 +0100

    First try

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,12 +3,12 @@
 /**
 * main - prints number of arguments passed into it
 *@argc: argument count
-*@argv: argument values
+*@argv: array of argument values
 *Return: value of argc
 */
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc-1);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
