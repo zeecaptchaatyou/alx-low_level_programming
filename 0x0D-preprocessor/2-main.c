73899116e48de393ab62626919903362cd0c418d
dev_zee <zeecaptcha@gmail.com>
2023-03-22 03:49:27 +0100

    It's name is all it tells

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /** 
 * main - prints the name of this file
 * @void: no arguments
 * Return: 0 if main is succesful
 */
-int main(void) 
-{
+
+int main(void) {
     printf("%s\n", __FILE__);
-    return (0);
+    return 0;
 }
