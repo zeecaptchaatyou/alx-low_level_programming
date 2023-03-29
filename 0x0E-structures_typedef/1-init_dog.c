b0c8c571ebff0d14dc38fbee7c99239c1067eebc
Zainab Jimoh <zeecaptcha@zee.local>
2023-03-30 00:36:17 +0100

    Mic Testing

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *@init_dog - function that initializes stuct dog
+ *@ *d - pointer to struct dog & argument 1
+ *@name - argument 2
+ *@age -argument 3
+ *@owner - argument 4
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)  {
+    (*d).name = name;
+    d->owner = owner;
+    d->age = age
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,8 +3,12 @@
 
 
 struct dog {
-    char name[7];
+    char *name;
     float age;
-    char owner[5];
+    char *owner;
 }
-#endif
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);#endif
