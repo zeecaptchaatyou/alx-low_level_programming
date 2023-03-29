a1cd010eee3b1bc5f9560428ac7aadb3dcff8271
Zainab Jimoh <zeecaptcha@zee.local>
2023-03-30 00:54:00 +0100

    Mic Testing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-
+/**
+ *  struct dog - the shape of a dog, lmao :)
+ *  @name - member 1
+ *  @age - member 2
+ *  @owner - member 3
+*/
 struct dog {
     char *name;
     float age;
     char *owner;
-}
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
