d5ddda4452f7d0271990d31b27106a9876925dd6
Zainab Jimoh <zeecaptcha@zee.local>
2023-04-01 14:51:32 +0100

    Came back to this

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints "_putchar", follwowed by a new line
+ * Return: 0, if program is sucessful.
+*/
+
+int main(void) {
+    printf("_putchar\n");
+    return 0;
+}
