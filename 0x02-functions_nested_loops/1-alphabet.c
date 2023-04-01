1c22375750f057312ef68c2369033e95cf3cc95b
Zainab Jimoh <zeecaptcha@zee.local>
2023-04-01 15:17:48 +0100

    Blah blah

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * print_alphabet - prints the alphabet in lower case
@@ -14,15 +13,4 @@ void print_alphabet()
         putchar(i);
     }
     putchar('\n');
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
 }
