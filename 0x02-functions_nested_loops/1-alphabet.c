40ee6a1b86da5723febac19743a84577eb7a6056
Zainab Jimoh <zeecaptcha@zee.local>
2023-04-01 15:16:59 +0100

    Blah blah

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,28 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * print_alphabet - Entry function
+ * print_alphabet - prints the alphabet in lower case
+ * Return: 0 if the program is successful
+*/
+
+void print_alphabet()
+{
+    int i;
+    for (i=97; i<123; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+}
+
+/**
+ * main - check the code
+ *
  * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-char c = 'a';
-
-while (c <= 'z')
-{
-_putchar(c);
-c++;
-}
-_putchar('\n');
+    print_alphabet();
+    return (0);
 }
