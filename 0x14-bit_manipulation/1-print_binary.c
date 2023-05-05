6a4b5e96170eecbe61dee71532b7710ec94aff40
Zainab Jimoh <zeecaptcha@zee.local>
2023-05-05 15:36:49 +0100

    ...

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * multiples - prints the multiple 2 
+*/
+/**
+ * print-binary - prints the binary representation of a number
+ * @n: number to be converted
+ * Return: 0
+*/
+void print_binary(unsigned long int n)
+{
+int i = 0;
+
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,6 @@
 #include <stddef.h>
 
 unsigned int binary_to_uint(const char *b);
-int _putchar(char c);
+void print_binary(unsigned long int n);
 
 #endif /*MAIN_H*/
