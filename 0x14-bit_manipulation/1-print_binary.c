7b38b2cc78731a5538044ba9f342fc86878bc4a8
Zainab Jimoh <zeecaptcha@zee.local>
2023-05-08 01:10:52 +0100

    Still on this

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,42 @@
 #include "main.h"
 
 /**
- * multiples - prints the multiple 2
+ * multiples - prints the highest possible multiple of 2 that is less than n
+ * @new_n: value to check for the highest multiple of 2 below
+ * Return: (multiple divided by 2
 */
+int multiples(int new_n)
+{
+int multiple = 1;
+while (multiple <= new_n)
+{
+multiple <<= 1;
+}
+return (multiple >> 1);
+}
+
 /**
- * print-binary - prints the binary representation of a number
+ * print_binary - prints the binary representation of a number
  * @n: number to be converted
  * Return: 0
 */
 void print_binary(unsigned long int n)
 {
-
+unsigned long int i = multiples(n);
+if (n == 0)
+{
+putchar(48);
 }
-/*I'm not done with this....obviously. Will come aback to it*/
+for ( ; i >= 1; i >>= 1)
+{
+if (n >= i)
+{
+putchar(49);
+n -= i;
+}
+else if (n < i)
+{
+putchar(48);
+}
+}
+}
