628129725ff611354458577f9218fbe61c2f47cd
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-07 14:06:53 +0100

    sixth task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,28 @@
 #include "main.h"
 
 /**
-* _sqrt_recursion - returns the natural square root of a number
-* @n: number to be used
-*
-* Return: the square root of n
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to toy with
+ * Return: literally what I said above: unsigned square root of n
 */
-
 int _sqrt_recursion(int n)
 {
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt(0, n));
-}
-
-/**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
- *
- * Return: the square root of n
- */
-
-int _sqrt(int n, int x)
+float i = 1, j = n, k = 0;
+if (n > 1)
 {
-	if (n > x / 2)
-		return (-1);
-	else if (n * n == x)
-		return (n);
-	return (_sqrt(n + 1, x));
+for ( ; i < j; i++)
+{
+k = n / i;
+if (k == i)
+break;
+}
+if (k != i)
+return (-1);
+else
+return (k);
+}
+else if (n < 1)
+return (n);
+else
+return (1);
 }
