8642df7ca3c88016063161f96b9240dc9ca5da1d
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-09 09:13:07 +0100

    Call me Zee

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,8 +11,8 @@ int main(int argc __attribute__((unused)), char **argv)
 int i = 0;
 for ( ; argv[0][i] != '\0'; i++)
 {
-putchar(argv[0][i]);
+_putchar(argv[0][i]);
 }
-putchar('\n');
+_putchar('\n');
 return (0);
 }
