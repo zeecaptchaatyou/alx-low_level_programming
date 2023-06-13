e801e5bc528aa36b4fc4df5d0d9f32617e0732d1
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-14 00:44:18 +0100

    third task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -38,24 +38,3 @@ for (c = 0; s2[c]!= '\0'; c++,i++)
 new_s[i] = s2[c];
 return (new_s);
 }
-
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s;
-
-    s = str_concat("Betty ", "Holberton");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
-}
