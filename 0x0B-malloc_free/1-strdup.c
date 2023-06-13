24c6729d9b52f18ebd2cf574861c0da65c180d26
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-14 00:43:12 +0100

    second task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,23 +20,3 @@ j++;
 }
 return (new_str);
 }
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s;
-
-    s = _strdup("Holberton");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
-}
