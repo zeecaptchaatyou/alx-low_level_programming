3e7afefb3e370ce4146434ffc58621b825eb27ea
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-14 00:48:49 +0100

    fixed memory issue

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,14 +9,35 @@
 */
 char *_strdup(char *str)
 {
-int i = 0, j = 0;
-char *new_str = (char*)malloc(i * sizeof(char));
+int i, j = 0;
+char *new_str;
 while (str[i] != '\0')
 i++;
+new_str = (char*)malloc(i * sizeof(char));
 while(str[j] != '\0')
 {
 new_str[j] = str[j];
 j++;
 }
 return (new_str);
+}
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s;
+
+    s = _strdup("HFirst, solve the problem. Then, write the code.");
+    if (s == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
