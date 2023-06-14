75bae91a5b28437229c19f99c2dc58b1b28f29b3
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-14 01:28:34 +0100

    DKWTsay

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,6 @@
  * @s: pointer to a string of characters
  * Return: length of the string upon success, else 0
 */
-
 int _strlen(char *s)
 {
 int length = 0;
@@ -26,8 +25,21 @@ char *str_concat(char *s1, char *s2)
 {
 int i = 0, s_len, c;
 char *new_s;
+
+/*Checking if s1 || s2 are null*/
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
 s_len = _strlen(s1) + _strlen(s2);
-new_s = (char *)malloc(s_len * sizeof(char));
+new_s = (char *)malloc((s_len * sizeof(char)) + 1);
+
+/*Handling malloc's return*/
+if (new_s == NULL)
+return (NULL);
+
+/*actual thing*/
 while (s1[i] != '\0')
 {
 new_s[i] = s1[i];
