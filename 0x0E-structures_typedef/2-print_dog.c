20d68ca0f756c2c09658b8d9360e4dd0dc6351cf
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-20 00:21:16 +0100

    the checker doesn't want two if statements

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,8 +8,8 @@
 */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-exit;
+if (d != NULL)
+{
 if (d->name == NULL)
 printf("Name: (nil)");
 else
@@ -22,4 +22,5 @@ if (d->owner == NULL)
 printf("Owner: (nil)");
 else
 printf("Owner: %s\n", d->owner);
+}
 }
