fa8af316cde70fee817d4f4142160d53ec5f9c6f
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-20 00:05:12 +0100

    added comments where needed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
  * dog - creates a dog
- * @name: first member
- * @age: second member
- * @owner: third member 
+ * @name: first member and name of dog
+ * @age: second member and age of dog
+ * @owner: third member and owner of dog
 */
 struct  dog 
 {
@@ -16,6 +17,9 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - a new instance of struct dog(also a typedef of struct dog)
+*/
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
