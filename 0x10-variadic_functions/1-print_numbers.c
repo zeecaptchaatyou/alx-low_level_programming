f6bc600d70a033eba30f8874b9e212a472f8ec74
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-22 18:41:51 +0100

    this is my (n + 1)th try ya'll

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,15 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 size_t i = 0;
 va_list(ments);
 va_start(ments, n);
-if (n > 0)
-{
+
 for ( ; i < n; i++)
 {
 printf("%d", va_arg(ments, int));
 if (separator != NULL && i != (n - 1))
 printf("%s ", separator);
 }
-}
-printf("\n");
 va_end(ments);
+printf("\n");
 }
