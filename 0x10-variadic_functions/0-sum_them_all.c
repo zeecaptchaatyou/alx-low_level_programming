e8518291c1b10ce8b1513c90d4b166f26df39c49
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-22 17:12:47 +0100

    Task 1 and 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,11 +11,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 va_list ments;
 size_t i = 0, sum = 0;
-if (n == 0)
-return (0);
+if (n != 0)
+{
 va_start(ments, n);
 for ( ; i < n; i++)
 sum += va_arg(ments, int);
 va_end(ments);
 return (sum);
+}
+return (0);
 }
