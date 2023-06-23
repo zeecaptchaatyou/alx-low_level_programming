ed9ddf576a814f8a3a76416a85cfa95e354a1643
Zainab Jimoh <zeecaptchat@gmail.com>
2023-06-23 08:57:19 +0100

    boy oh boy

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,16 +23,3 @@ printf("%s", separator);
 printf("\n");
 va_end(ments);
 }
-
-#include "variadic_functions.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-   print_numbers("; ", 5, 30, 938, -123024, 43402, 0);
-    return (0);
-}
