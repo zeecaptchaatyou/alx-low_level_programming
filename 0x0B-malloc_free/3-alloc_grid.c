9688501e360259fc779f704b8d32ea7d569caf10
zeecaptchaatyou <cocolitalee@gmail.com>
2023-08-10 00:13:16 +0100

    fixed malloc's return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,13 +12,14 @@ int **alloc_grid(int width, int height)
 {
 int i = 0, j = 0;
 int **arr = (int **)malloc(height * sizeof(int *));
-if (height <= 0 || width <= 0)
+
+if (arr == NULL)
 {
 free(arr);
 return (NULL);
 }
 
-if (arr == NULL)
+if (height <= 0 || width <= 0)
 {
 free(arr);
 return (NULL);
