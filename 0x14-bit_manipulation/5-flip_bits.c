68091f4bda584956244a542a23f19d99618e1924
Zeecaptchaatyou <zeecaptcha@gmail.com>
2023-09-01 05:50:28 +0100

    the checker if faulty today soooo

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,5 +9,31 @@
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+unsigned long int a = n ^ m, bits = 0;
 
+for ( ; a; a >>= 1)
+{
+if (a & 1)
+bits += 1;
+}
+return (bits); }
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    unsigned int n;
+
+    n = flip_bits(1024, 1);
+    printf("%u\n", n);
+    n = flip_bits(402, 98);
+    printf("%u\n", n);
+    n = flip_bits(1024, 3);
+    printf("%u\n", n);
+    n = flip_bits(1024, 1025);
+    printf("%u\n", n);
+    return (0);
 }
