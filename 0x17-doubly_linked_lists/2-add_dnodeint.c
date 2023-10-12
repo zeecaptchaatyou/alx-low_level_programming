cd5ccf018380f420d328b245b7c44fe079b80112
Zainnyba <ayomiposijimoh@gmail.com>
2023-10-12 21:38:44 +0100

    adds  new node to a doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,10 +8,8 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new_node;
+dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
-
-new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 
@@ -19,6 +17,8 @@ new_node->n = n;
 new_node->prev = NULL;
 new_node->next = *head;
 
+if (*head != NULL)
+(*head)->prev = new_node;
 *head = new_node;
 
 return (new_node);
