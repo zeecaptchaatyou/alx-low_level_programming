a8f87739e57e09f2bb627dc27f93c59caf900cd8
Zainnyba <ayomiposijimoh@gmail.com>
2023-10-12 19:12:03 +0100

    I've missed C!

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,18 +7,16 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-const dlistint_t *head;
-size_t n = 0;
+size_t n = 0; /*number of nodes*/
 
 if (h == NULL)
-return (EXIT_FAILURE);
+return (n);
 
-head = h;
-while (head->next != NULL)
+while (h != NULL)
 {
-printf("%d\n", head->n);
-head = head->next, n++;
+printf("%d\n", h->n);
+h = h->next, n++;
 }
-printf("%d\n", head->n);
-return (n + 1);
+
+return (n);
 }
