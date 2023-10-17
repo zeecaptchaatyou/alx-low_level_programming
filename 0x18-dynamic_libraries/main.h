95c6b852c3b4483c5e765b8eec723903d0dbc1a2
Zainnyba <ayomiposijimoh@gmail.com>
2023-10-17 04:21:28 +0100

    yeah, I'm done!

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,28 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int nrandom();
+void _puts(char *s);
+int _atoi(char *s);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+char *_strchr(char *s, char c);
+int _strcmp(char *s1, char *s2);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+char *_memset(char *s, char b, unsigned int n);
+/*beautiful letter "E", don't you think?*/
+
+#endif /* __MAIN__ */
